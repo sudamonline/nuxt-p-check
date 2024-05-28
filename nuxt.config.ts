36d022947bf751df856172e092085503059b7da3
sudamonline <sudamonline@gmail.com>
2024-05-28 16:56:21 +0530

    changed tailwind install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  modules: ['@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
